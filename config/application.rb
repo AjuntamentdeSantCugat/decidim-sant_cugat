225e963296160293afd75604f42a86ce2c965810
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-10-22 17:59:48 +0200

    Add dependency with fog/aws

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require "decidim/rails"
 require "action_cable/engine"
+require 'fog/aws'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
