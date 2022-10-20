b8d36b86c02e57a4c4a8cfc72148b859e73bf1b1
Fernando Blat <fernando@blat.es>
2022-10-20 06:13:01 +0200

    Refactor

--- app/services/participatory_process_picker_2022.rb
+++ app/services/participatory_process_picker_2022.rb
@@ -2,10 +2,10 @@
 
 class ParticipatoryProcessPicker2022
 
-  PROCESS_GROUP_URL = '/processes_groups/3'
   PROCESS_GROUP_ID = 3
+  PROCESS_GROUP_URL = "/processes_groups/#{PROCESS_GROUP_ID}"
 
-  attr_reader :current_user
+  attr_reader :current_user, :district_council
 
   MAPPING = {
     "CB1" => "/processes/nucliantic2022",
@@ -19,30 +19,18 @@ class ParticipatoryProcessPicker2022
 
   def initialize(current_user)
     @current_user = current_user
-    @distrinct_council = nil
+    @district_council = authorization_metadata.dig("district_council")
 
-    if current_user.nil? || current_user.extended_data.nil?
-      Rails.logger.info "[ParticipatoryProcessPicker2022] User extended data not found"
-      return
-    end
-
-    authorizations = current_user.extended_data.dig("authorizations")
-    if authorizations
-      if authorization = authorizations.find{ |a| a["name"] == "census_authorization_handler" }
-        @district_council = authorization.dig("metadata", "district_council")
-      end
-    end
-    Rails.logger.info "[ParticipatoryProcessPicker2022] Authorizations: #{authorizations}"
-    Rails.logger.info "[ParticipatoryProcessPicker2022] District council: #{@district_council}"
+    Rails.logger.info "[ParticipatoryProcessPicker2022] Authorizations: #{authorization_metadata}"
+    Rails.logger.info "[ParticipatoryProcessPicker2022] District council: #{district_council}"
   end
 
   def process_url
-    if @district_council.nil?
+    if district_council.blank?
       if current_user.nil?
         return host + PROCESS_GROUP_URL
       else
-        authorizations = current_user.extended_data.dig("authorizations")
-        if authorizations.nil? || authorizations.empty?
+        if authorization_metadata.blank?
           redirect_url = host + "/parcicipatory_process_redirect"
           return host + "/authorizations/new?handler=census_authorization_handler&redirect_url=#{redirect_url}"
         else
@@ -51,24 +39,13 @@ class ParticipatoryProcessPicker2022
         end
       end
     else
-      return host + (MAPPING[@district_council] || PROCESS_GROUP_URL)
-    end
-  end
-
-  def host
-    case Rails.env
-    when "production"
-      "https://decidim.santcugat.cat"
-    when "staging"
-      "https://decidim-sant-cugat.populate.tools"
-    else
-      "http://localhost:3000"
+      return host + (MAPPING[district_council] || PROCESS_GROUP_URL)
     end
   end
 
   def component_id
     @component_id ||= begin
-      slug = if @distrinct_council.nil?
+      slug = if district_council.blank?
                MAPPING.values.first.split("/").last.strip
              else
                process_url.split("/").last.strip
@@ -90,5 +67,40 @@ class ParticipatoryProcessPicker2022
 
   def process_group
     @process_group ||= Decidim::ParticipatoryProcessGroup.find(PROCESS_GROUP_ID)
+  end
+
+  def host
+    case Rails.env
+    when "production"
+      "https://decidim.santcugat.cat"
+    when "staging"
+      "https://decidim-sant-cugat.populate.tools"
+    else
+      "http://localhost:3000"
+    end
+  end
+
+  def authorization_metadata
+    return {} if current_user.nil?
+
+    if current_user.extended_data.empty?
+      decidim_authorization_metadata
+    elsif authorizations = current_user.extended_data.dig("authorizations")
+      if authorization = authorizations.find{ |a| a["name"] == "census_authorization_handler" }
+        authorization.dig(:metadata)
+      else
+        {}
+      end
+    else
+      {}
+    end
+  end
+
+  def decidim_authorization_metadata
+    return nil if current_user.nil?
+
+    if authorization = Decidim::Authorization.where(decidim_user_id: current_user.id).first
+      authorization.metadata
+    end
   end
 end
