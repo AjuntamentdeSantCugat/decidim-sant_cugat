cc5788c6852a6e9d0f25b5e12614868d2dd093f0
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-04-22 19:43:12 +0200

    Fix name and path of controller

--- app/controllers/decidim/participatory_process_picker_controller.rb
+++ app/controllers/decidim/participatory_process_picker_controller.rb
@@ -1,7 +1,7 @@
 module Decidim
   class ParticipatoryProcessPickerController < DecidimController
     def redirect
-      ppp = ParticipatoryProcessPicker2022.new(current_user)
+      ppp = ::ParticipatoryProcessPicker2022.new(current_user)
       redirect_to ppp.process_url and return false
     end
   end
